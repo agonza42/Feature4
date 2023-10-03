d2cea8964a0b1d7bcb2d877f0ce52e2c76e786df
agonza42 <agonza42@nd.edu>
2023-10-03 17:36:32 -0400

    Adding Back4App Keys

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Main from "./Components/Main/Main";
 import * as Env from "./environments";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import Parse from "parse";
 
 Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
 Parse.serverURL = Env.SERVER_URL;
+
+//Before using the SDK...
+Parse.setAsyncStorage(AsyncStorage);
 
 function App() {
   return <Main />;
