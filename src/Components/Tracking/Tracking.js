fb2788816440652e4acab0774a7c247829d0c2b8
agonza42 <agonza42@nd.edu>
2023-10-04 18:45:28 -0400

    Tracking Entry to Back4App Working

--- src/Components/Tracking/Tracking.js
+++ src/Components/Tracking/Tracking.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { createTrackingEntry } from '../../Common/Services/TrackingService.js';
 import TrackingChild from "./TrackingChild";
 
 // Import the CSS file
@@ -25,7 +26,7 @@ class Tracking extends Component {
   }
   
   // Function to handle events that change the input
-  handleInputChange(event) {
+  /*handleInputChange(event) {
     // Logging into the console to make sure form is getting correct values
     console.log('Event Target:', event.target);
     console.log('Name:', event.target.name);
@@ -42,11 +43,28 @@ class Tracking extends Component {
     this.setState({ formData: updatedFormData }, () => {
       console.log("Updated state:", this.state.formData);
     });*/
-  }
+  /*}*/
+  handleInputChange(event) {
+    // Logging into the console to make sure the form is getting the correct values
+    console.log('Event Target:', event.target);
+    console.log('Name:', event.target.name);
+    console.log('Value:', event.target.value);
+
+    const { name, value } = event.target;
+
+    this.setState(prevState => ({
+        formData: {
+            ...prevState.formData,
+            [name]: name === 'todaysDate' || name === 'breakfastCals' ? parseFloat(value) : value || name === 'lunchCals' ? parseFloat(value) : value || name === 'dinnerCals' ? parseFloat(value) : value || name === 'snacksCals' ? parseFloat(value) : value || name === 'exerciseCals' ? parseFloat(value) : value
+        }
+    }), () => {
+        console.log('Updated state:', this.state.formData);
+    });
+}
 
   // Function to handle asynchronous data when a submit event occurs
   // CHANGE to take in data from the form submission to then send to a JSON fil
-  async handleSubmit(event) {
+  /*async handleSubmit(event) {
     event.preventDefault();
     // Alert for submit button
     alert('Tracking form button works');
@@ -68,6 +86,40 @@ class Tracking extends Component {
     } catch (error) {
       console.error('Error fetching data:', error);
     }
+  }*/
+  async handleSubmit(event) {
+    event.preventDefault();
+    console.log("Submitting form data:", this.state.formData);
+
+    // Parse the values
+    const todaysDateValue = (this.state.formData.todaysDate);
+    const breakfastCalsValue = parseFloat(this.state.formData.breakfastCals);
+    const lunchCalsValue = parseFloat(this.state.formData.lunchCals);
+    const dinnerCalsValue = parseFloat(this.state.formData.dinnerCals);
+    const snacksCalsValue = parseFloat(this.state.formData.snacksCals);
+    const exerciseCalsValue = parseFloat(this.state.formData.exerciseCals);
+    
+    // Simple validation
+    if (isNaN(todaysDateValue) || isNaN(breakfastCalsValue) || isNaN(lunchCalsValue) || isNaN(dinnerCalsValue) || isNaN(snacksCalsValue) || isNaN(exerciseCalsValue)) {
+      alert('Please provide values for all fields.');
+      return;
+    }
+
+    const trackingEntryData = {
+      todaysDate: todaysDateValue,
+      breakfastCals: breakfastCalsValue,
+      lunchCals: lunchCalsValue,
+      dinnerCals: dinnerCalsValue,
+      snacksCals: snacksCalsValue,
+      exerciseCals: exerciseCalsValue
+    };
+
+    try {
+      const response = await createTrackingEntry(trackingEntryData);
+      console.log('Tracking entry created successfully:', response);
+    } catch (error) {
+      console.error('Error creating the tracking entry:', error);
+    }
   }
 
   // Using render for the component's HTML including form and button for user interaction
@@ -87,7 +139,7 @@ class Tracking extends Component {
             formData={this.state.formData}
             onChange={this.handleInputChange}
           />
-          <button type="submit" className="btn btn-primary">
+          <button type="submit">
             Submit
           </button>
         </form>
@@ -95,7 +147,6 @@ class Tracking extends Component {
     );
   }
 }
-  
 
 export default Tracking;
   
